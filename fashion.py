867b385a164aec9f369c93e440406f90cca8f923
Sarinder Virk <savirk@ucdavis.edu>
2018-11-22 23:58:32 -0800

    visualize 10x10 images

--- fashion.py
+++ fashion.py
@@ -1,27 +1,23 @@
+#
+# This code implements the 6 steps required to complete HW2:
+# https://gradebot.org/course/ecs235a/18f/homework/2/
+#
+# - Step 1 -
 # Dependencies:
-# pip3 install tensorflow
-# pip3 install keras
-# pip3 install git+https://github.com/sarinderv/cleverhans.git@fashion-mnist#egg=cleverhans
+# pip install tensorflow
+# pip install keras
+# pip install cleverhans
 
 # TensorFlow and tf.keras
 import tensorflow as tf
-#from tensorflow import keras
 import keras
+from keras.datasets import fashion_mnist
 
 # Helper libraries
 import numpy as np
-#import matplotlib
 import matplotlib.pyplot as plt
 from cleverhans.attacks import BasicIterativeMethod
-from cleverhans.dataset import Fashion_MNIST
-from cleverhans.loss import CrossEntropy
-from cleverhans.train import train
-from cleverhans.utils import pair_visual, grid_visual, AccuracyReport
-from cleverhans.utils_keras import cnn_model
 from cleverhans.utils_keras import KerasModelWrapper
-from cleverhans.utils_tf import model_eval
-
-#matplotlib.use('agg') # for X11
 
 print('Tensorflow version:', tf.__version__)
 
@@ -30,7 +26,6 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 # load fashion dataset
-fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
 # scale pixel values (0,255) => (0,1)
@@ -40,15 +35,12 @@ test_images = test_images / 255.0
 # Obtain Image Parameters
 img_rows, img_cols = train_images.shape[1:3]
 nchannels = 1 # greyscale
-nb_classes = 10 #train_labels.shape[1]
+nb_classes = 10
+nb_iter = 10
 
 print('Images rows: {} columns: {}'.format(img_rows, img_cols))
-print('Train images shape:', train_images.shape)
-print('Train labels shape:', train_labels.shape)
-print('Test images shape:', test_images.shape)
-print('Test labels shape:', test_labels.shape)
 
-# build a simple model
+# - Step 2 - build a simple model
 if (0):
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),
@@ -71,15 +63,15 @@ keras.backend.set_session(sess)
 
 # reshape input from (28,28) to (28,28,1) - first layer Conv2D wants 4D tensor as input
 train_images = train_images.reshape(train_images.shape[0], 28, 28, nchannels)
-test_images = test_images.reshape(test_images.shape[0], 28, 28, nchannels)
+test_images2 = test_images.reshape(test_images.shape[0], 28, 28, nchannels)
 
 filename = 'model.h5'
-try: # load a previously trained model
-  model = tf.keras.models.load_model(filename)
+try:  # load a previously trained model
+  model = keras.models.load_model(filename)
   print("Model loaded from: {}".format(filename))
 except OSError as e:
   print(e)
-  # build a model which can have >90% accuracy
+  # - Step 3 - build a model which can have >90% accuracy
   model = keras.Sequential([
     keras.layers.Conv2D(32, (2,2), padding='same', activation=tf.nn.relu, input_shape=(28, 28, 1)),
     keras.layers.MaxPool2D(padding='same'),
@@ -98,16 +90,37 @@ except OSError as e:
 model.summary()
 
 # evaluate accuracy
-test_loss, test_acc = model.evaluate(test_images, test_labels)
+test_loss, test_acc = model.evaluate(test_images2, test_labels)
 print('Test accuracy:', test_acc)
 
+# - Step 4 - Implement untargeted attack using the Basic Iterative Method
 wrap = KerasModelWrapper(model)
 bim = BasicIterativeMethod(wrap, sess=sess)
 bim_params = {'eps': 0.3,
               'eps_iter': 0.05,
-              'nb_iter': 10,
+              'nb_iter': nb_iter,
               'clip_min': 0.,
               'clip_max': 1.}
+
+# - Step 5 - From each category in the Fashion-MNIST test set, select 10 images
+# to generate adversarial examples using 5 and 10 iterations, respectively.
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}%\n({})".format(class_names[predicted_label],
+                                         100 * np.max(predictions_array),
+                                         i), color=color)
 
 # Visualize adversarial images from a trained network
 def adv_viz(attack, attack_params):
@@ -137,11 +150,19 @@ def adv_viz(attack, attack_params):
   fig.savefig('grid_viz.png')
   return adv_images
 
-# Display a grid of some adversarial examples
-adv_images = adv_viz(bim, bim_params)
+predictions = model.predict(test_images2)
 
-preds = model.predict(adv_images)
-print('Predictions:', preds)
-
-test_loss, test_acc = model.evaluate(adv_images, list(range(nb_classes)))
-print('Test accuracy:', test_acc)
+# Plot the 10 test images from each category, their predicted label, and the true label
+# Color correct predictions in blue, incorrect predictions in red
+pos = 0
+plt.figure(figsize=(20, 20))
+for clz in range(nb_classes):
+    idxs = np.where(test_labels == clz)[0][:10]
+    for i in idxs:
+      adv_inputs = test_images[idxs]
+      #adv_images = bim.generate_np(adv_inputs, **bim_params)
+      plt.subplot(10, 10, pos+1)
+      pos += 1
+      plot_image(i, predictions, test_labels, test_images)
+plt.savefig('original-images.png'.format(nb_iter))
+#plt.savefig('{}-iterations.png'.format(nb_iter))
