79cb39d7a52375ccf7454233dbbe592450b7b735
Sarinder Virk <savirk@ucdavis.edu>
2018-11-24 00:10:37 -0800

    format per homework requirements

--- fashion.py
+++ fashion.py
@@ -2,27 +2,24 @@
 # This code implements the 6 steps required to complete HW2:
 # https://gradebot.org/course/ecs235a/18f/homework/2/
 #
-# - Step 1 -
+# - Step 1 - Install TensorFlow.
 # Dependencies:
 # pip install tensorflow
 # pip install keras
 # pip install cleverhans
 
-# TensorFlow and tf.keras
+import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
 import keras
-from keras.datasets import fashion_mnist
-
-# Helper libraries
-import numpy as np
-import matplotlib.pyplot as plt
 from cleverhans.attacks import BasicIterativeMethod
 from cleverhans.utils_keras import KerasModelWrapper
+from keras.datasets import fashion_mnist
 
 print('Tensorflow version:', tf.__version__)
 
 # map class indices to names
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 # load fashion dataset
@@ -34,13 +31,13 @@ test_images = test_images / 255.0
 
 # Obtain Image Parameters
 img_rows, img_cols = train_images.shape[1:3]
-nchannels = 1 # greyscale
+nchannels = 1  # greyscale
 nb_classes = 10
 nb_iter = 10
 
 print('Images rows: {} columns: {}'.format(img_rows, img_cols))
 
-# - Step 2 - build a simple model
+# - Step 2 - Implement the basic classification neural network.
 if (0):
     model = keras.Sequential([
         keras.layers.Flatten(input_shape=(28, 28)),
@@ -48,7 +45,7 @@ if (0):
         keras.layers.Dense(10, activation=tf.nn.softmax)
     ])
     model.summary()
-    model.compile(optimizer=tf.train.AdamOptimizer(), 
+    model.compile(optimizer=tf.train.AdamOptimizer(),
                   loss='sparse_categorical_crossentropy',
                   metrics=['accuracy'])
     model.fit(train_images, train_labels, epochs=5)
@@ -67,25 +64,25 @@ test_images2 = test_images.reshape(test_images.shape[0], 28, 28, nchannels)
 
 filename = 'model.h5'
 try:  # load a previously trained model
-  model = keras.models.load_model(filename)
-  print("Model loaded from: {}".format(filename))
+    model = keras.models.load_model(filename)
+    print("Model loaded from: {}".format(filename))
 except OSError as e:
-  print(e)
-  # - Step 3 - build a model which can have >90% accuracy
-  model = keras.Sequential([
-    keras.layers.Conv2D(32, (2,2), padding='same', activation=tf.nn.relu, input_shape=(28, 28, 1)),
-    keras.layers.MaxPool2D(padding='same'),
-    keras.layers.Conv2D(32, (2,2), padding='same', input_shape=(28, 28, 1)),
-    keras.layers.MaxPool2D(padding='same'),
-    keras.layers.Flatten(),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
-  ])
-  model.compile(optimizer=keras.optimizers.Adam(), 
-                loss='sparse_categorical_crossentropy',
-                metrics=['accuracy'])
-  model.fit(train_images, train_labels, epochs=5)
-  model.save(filename)
+    print(e)
+    # - Step 3 - Improve the classification accuracy above 90% by adding 2D convolution layers and max pooling layers.
+    model = keras.Sequential([
+        keras.layers.Conv2D(32, (2, 2), padding='same', activation=tf.nn.relu, input_shape=(28, 28, 1)),
+        keras.layers.MaxPool2D(padding='same'),
+        keras.layers.Conv2D(32, (2, 2), padding='same', input_shape=(28, 28, 1)),
+        keras.layers.MaxPool2D(padding='same'),
+        keras.layers.Flatten(),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+    model.compile(optimizer=keras.optimizers.Adam(),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit(train_images, train_labels, epochs=5)
+    model.save(filename)  # Save your model in case of retraining.
 
 model.summary()
 
@@ -96,73 +93,58 @@ print('Test accuracy:', test_acc)
 # - Step 4 - Implement untargeted attack using the Basic Iterative Method
 wrap = KerasModelWrapper(model)
 bim = BasicIterativeMethod(wrap, sess=sess)
-bim_params = {'eps': 0.3,
+bim_params = {'eps': 0.2,
               'eps_iter': 0.05,
               'nb_iter': nb_iter,
               'clip_min': 0.,
               'clip_max': 1.}
 
-# - Step 5 - From each category in the Fashion-MNIST test set, select 10 images
-# to generate adversarial examples using 5 and 10 iterations, respectively.
-def plot_image(i, predictions_array, true_label, img):
-    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
-    plt.grid(False)
-    plt.xticks([])
-    plt.yticks([])
 
-    plt.imshow(img, cmap=plt.cm.binary)
+# - Step 5 - From each category in the Fashion-MNIST test set,
+# select 10 images to generate adversarial examples using 5 and 10 iterations, respectively.
+orig_images = np.empty([0, 28, 28, 1])
+orig_labels = np.empty([0])
+for clz in range(nb_classes):
+    idxs = np.where(test_labels == clz)[0][:10]
+    orig_images = np.append(orig_images, test_images2[idxs], axis=0)
+    orig_labels = np.append(orig_labels, test_labels[idxs], axis=0)
 
-    predicted_label = np.argmax(predictions_array)
-    if predicted_label == true_label:
+adv_images = bim.generate_np(orig_images, **bim_params)
+
+# Compute the average distortion introduced by the algorithm
+percent_perturbed = np.mean(np.sum((adv_images - orig_images) ** 2,
+                                   axis=(1, 2, 3)) ** .5)
+print('Avg. L_2 norm of perturbations {0:.4f}'.format(percent_perturbed))
+
+# Evaluate accuracy on adversarial images
+adv_loss, adv_acc = model.evaluate(adv_images, orig_labels)
+orig_loss, orig_acc = model.evaluate(orig_images, orig_labels)
+print('Adv accuracy: {}, Original accuracy: {}'.format(adv_acc, orig_acc))
+
+
+# - Step 6 - Use your model trained in Step 3 to classify the adversarial examples.
+adv_preds = model.predict(adv_images)
+orig_preds = model.predict(orig_images)
+
+# Plot the 10 test images from each category, the true label, and their predicted labels.
+# Color correct predictions in blue, incorrect predictions in red
+fig = plt.figure(figsize=(5, 100))
+plt.grid(False)
+plt.xticks([])
+plt.yticks([])
+for i, img in enumerate(orig_images):
+    fig.add_subplot(100, 2, i * 2 + 1)
+    plt.imshow(np.squeeze(img, axis=2), cmap=plt.cm.binary)
+    plt.ylabel("{}".format(class_names[int(orig_labels[i])]), rotation=0)
+for i, img in enumerate(adv_images):
+    fig.add_subplot(100, 2, i * 2 + 2)
+    plt.imshow(np.squeeze(img, axis=2), cmap=plt.cm.binary)
+    adv_pred_label = np.argmax(adv_preds[i])
+    if adv_pred_label == orig_labels[i]:
         color = 'blue'
     else:
         color = 'red'
+    plt.ylabel("{} {:2.0f}%".format(class_names[adv_pred_label],
+                                    100 * np.max(adv_preds[i])), color=color, rotation=0)
 
-    plt.xlabel("{} {:2.0f}%\n({})".format(class_names[predicted_label],
-                                         100 * np.max(predictions_array),
-                                         i), color=color)
-
-# Visualize adversarial images from a trained network
-def adv_viz(attack, attack_params):
-  # Generate adversarial images ...
-  idxs = [np.where(test_labels == i)[0][0]
-                for i in range(nb_classes)]
-  adv_inputs = test_images[idxs]
-  adv_images = attack.generate_np(adv_inputs, **attack_params)
-
-  # Initialize our array for grid visualization
-  grid_shape = (nb_classes, 2, img_rows, img_cols, nchannels)
-  grid_viz_data = np.zeros(grid_shape, dtype='f')
-  for j in range(nb_classes):
-    grid_viz_data[j, 0] = adv_inputs[j]
-    grid_viz_data[j, 1] = adv_images[j]
-  print('grid_viz_data.shape=', grid_viz_data.shape)
-
-  # Compute the average distortion introduced by the algorithm
-  percent_perturbed = np.mean(np.sum((adv_images - adv_inputs)**2,
-                                     axis=(1, 2, 3))**.5)
-  print('Avg. L_2 norm of perturbations {0:.4f}'.format(percent_perturbed))
-
-  # Finally, block & display a grid of all the adversarial examples
-  #import matplotlib.pyplot as plt
-  from cleverhans.plot.pyplot_image import grid_visual
-  fig = grid_visual(grid_viz_data)
-  fig.savefig('grid_viz.png')
-  return adv_images
-
-predictions = model.predict(test_images2)
-
-# Plot the 10 test images from each category, their predicted label, and the true label
-# Color correct predictions in blue, incorrect predictions in red
-pos = 0
-plt.figure(figsize=(20, 20))
-for clz in range(nb_classes):
-    idxs = np.where(test_labels == clz)[0][:10]
-    for i in idxs:
-      adv_inputs = test_images[idxs]
-      #adv_images = bim.generate_np(adv_inputs, **bim_params)
-      plt.subplot(10, 10, pos+1)
-      pos += 1
-      plot_image(i, predictions, test_labels, test_images)
-plt.savefig('original-images.png'.format(nb_iter))
-#plt.savefig('{}-iterations.png'.format(nb_iter))
+plt.savefig('{}-iterations.png'.format(nb_iter))
